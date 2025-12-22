762aee23c3e335f2409d5543b78c7352e18a972d
elias-bm <eliasbabaalian@gmail.com>
2025-12-21 19:51:45 -0800

    copy static directory to public

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
-from textnode import TextNode, TextType
+import os, shutil
+
+def copy_dir(src, dst):
+    files = os.listdir(src)
+    for file in files:
+        src_path = os.path.join(src, file)
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dst)
+        else:
+            dst_path = os.path.join(dst, file)
+            os.mkdir(dst_path)
+            copy_dir(src_path, dst_path)
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+    copy_dir("static", "public")
 
 main()
